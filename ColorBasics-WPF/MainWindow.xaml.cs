05a3b09ae99dd87cfabd2862051a0429908c88cd
Samson Gama <samsongama@gmail.com>
2015-05-08 12:18:57 -0700

    Color and Depth in one window with FPS bar

--- ColorBasics-WPF/MainWindow.xaml.cs
+++ ColorBasics-WPF/MainWindow.xaml.cs
@@ -32,23 +32,34 @@ namespace Microsoft.Samples.Kinect.ColorBasics
         private KinectSensor kinectSensor = null;
 
         /// <summary>
-        /// Reader for color frames
-        /// </summary>
-        private ColorFrameReader colorFrameReader = null;
-
-        /// <summary>
         /// Bitmap to display
         /// </summary>
         private WriteableBitmap colorBitmap = null;
+        private WriteableBitmap depthBitmap = null;
 
         /// <summary>
         /// Current status text to display
         /// </summary>
         private string statusText = null;
+        private string fpsText = null;
 
         private ColorImageFormat colorImageFormat = ColorImageFormat.Bgra;
         private FrameDescription colorFrameDescription;
         private FrameDescription depthFrameDescription;
+        private MultiSourceFrameReader multiSourceFrameReader;
+
+        //Depth Frame
+        private ushort[] depthFrameData = null;
+        private byte[] depthPixels = null;
+        private const int BytesPerPixel = 4; // Size of the RGB pixel in the bitmap
+
+        //check performance in ticks
+        private const long TICKS_PER_SECOND = 10000000; //according to msdn
+        private double fps = 0;
+        private long prevTick = 0, ticksNow = 0;
+        private double sumFps = 0;
+        private int counter = 0;
+
 
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
@@ -57,15 +68,23 @@ namespace Microsoft.Samples.Kinect.ColorBasics
         {
             this.kinectSensor = KinectSensor.GetDefault(); // get the kinectSensor object
 
-            this.colorFrameReader = this.kinectSensor.ColorFrameSource.OpenReader(); // open the reader for the color frames
-            this.colorFrameReader.FrameArrived += this.Reader_ColorFrameArrived;  // wire handler for frame arrival
-            FrameDescription colorFrameDescription = this.kinectSensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);// create the colorFrameDescription from the ColorFrameSource using Bgra format
-            this.colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null); // create the bitmap to display
+            this.colorFrameDescription = this.kinectSensor.ColorFrameSource.CreateFrameDescription(this.colorImageFormat);
+            this.depthFrameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
+
+            this.depthFrameData = new ushort[depthFrameDescription.Width * depthFrameDescription.Height];
+            this.depthPixels = new byte[depthFrameDescription.Width * depthFrameDescription.Height * BytesPerPixel];
+
+            this.colorBitmap = new WriteableBitmap(this.colorFrameDescription.Width, this.colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null); // create the bitmap to display  
+            this.depthBitmap = new WriteableBitmap(this.depthFrameDescription.Width, this.depthFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
+            this.multiSourceFrameReader = this.kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth);
+
+            this.multiSourceFrameReader.MultiSourceFrameArrived += MultiSourceFrameReader_MultiSourceFrameArrived;
 
             this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged; // set IsAvailableChanged event notifier
 
             this.kinectSensor.Open(); // open the sensor
 
+            this.fpsText = "FPS = 0";
             this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText : Properties.Resources.NoSensorStatusText; // set the status text
             this.DataContext = this; // use the window object as the view model in this simple example
             this.InitializeComponent(); // initialize the components (controls) of the window
@@ -94,26 +113,34 @@ namespace Microsoft.Samples.Kinect.ColorBasics
         /// </summary>
         /// <param name="sender">object sending the event</param>
         /// <param name="e">event arguments</param>
-        private void Reader_ColorFrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        void MultiSourceFrameReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
         {
-            // ColorFrame is IDisposable
-            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
+            MultiSourceFrame reference = e.FrameReference.AcquireFrame();
+
+            if (reference == null)
             {
+                Debug.WriteLine("Send to debug output.");
+                return;
+            }
+            
+            // ColorFrame is IDisposable
+            using (ColorFrame colorFrame = reference.ColorFrameReference.AcquireFrame())
+            using (DepthFrame depthFrame = reference.DepthFrameReference.AcquireFrame())
+            {
+                
                 if (colorFrame != null)
                 {
                     FrameDescription colorFrameDescription = colorFrame.FrameDescription;
 
                     using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
                     {
-                        this.colorFrameReader.IsPaused = true;
 
                         // verify data and write the new color frame data to the display bitmap
                         if ((colorFrameDescription.Width == this.colorBitmap.PixelWidth) && (colorFrameDescription.Height == this.colorBitmap.PixelHeight))
                         {
-                            byte[] pixels = new byte[colorFrameDescription.Height * colorFrameDescription.Width * 4];
-                            colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
-                            int stride = colorFrameDescription.Width * 4;
-                            BitmapSource source = BitmapSource.Create(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride);
+                            BitmapSource source = getColorImage(colorFrameDescription, colorFrame);
+                            Bitmap bitmap = getBitmap(source);
+                            OpenCV(ref bitmap);
 
                             Bitmap bitmap;
                             using (MemoryStream outStream = new MemoryStream())
@@ -128,30 +155,83 @@ namespace Microsoft.Samples.Kinect.ColorBasics
                             
                             writeToBackBuffer(ConvertBitmap(bitmap));
                             bitmap.Dispose();
-                           
-                            
                         }
 
-                        this.colorFrameReader.IsPaused = false;
                     }
                 }
+                this.multiSourceFrameReader.IsPaused = false;
             }
+
         }
 
-        private void writeToBackBuffer(BitmapSource source)
+        public BitmapSource ToBitmap(DepthFrame frame)
         {
-            this.colorBitmap.Lock();
+            //System.Windows.Media.PixelFormat _format = System.Windows.Media.PixelFormats.Bgr32;
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+
+            ushort[] depthData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(depthData);
+
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < depthData.Length; ++depthIndex)
+            {
+                ushort depth = depthData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixelData[colorIndex++] = intensity; // Blue
+                pixelData[colorIndex++] = intensity; // Green
+                pixelData[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * PixelFormats.Bgr32.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixelData, stride);
+        }
+
+        private void writeToBackBuffer(BitmapSource source, WriteableBitmap bitmap)
+        {
+            bitmap.Lock();
             int stride = source.PixelWidth * (source.Format.BitsPerPixel / 8); // Calculate stride of source
             byte[] data = new byte[stride * source.PixelHeight]; // Create data array to hold source pixel data
             source.CopyPixels(data, stride, 0); // Copy source image pixels to the data array
 
             // Write the pixel data to the WriteableBitmap.
-            this.colorBitmap.WritePixels(new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight), data, stride, 0);
-            this.colorBitmap.AddDirtyRect(new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight));
-            this.colorBitmap.Unlock();
+            bitmap.WritePixels(new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight), data, stride, 0);
+            bitmap.AddDirtyRect(new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight));
+            bitmap.Unlock();
         }
 
-        private void openCV(ref Bitmap bitmap)
+        private BitmapSource getColorImage(FrameDescription colorFrameDescription, ColorFrame colorFrame)
+        {
+            byte[] pixels = new byte[colorFrameDescription.Height * colorFrameDescription.Width * 4];
+            colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            int stride = colorFrameDescription.Width * 4;
+            return BitmapSource.Create(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96, PixelFormats.Bgr32, null, pixels, stride);
+        }
+
+        private Bitmap getBitmap(BitmapSource source)
+        {
+            Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(source));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+
+            return bitmap;
+        }
+
+        private void OpenCV(ref Bitmap bitmap)
         {
             Mat testMat = BitmapConverter.ToMat(bitmap);
             MatOfDouble mu = new MatOfDouble();
@@ -160,7 +240,6 @@ namespace Microsoft.Samples.Kinect.ColorBasics
             double mean = mu.GetArray(0, 0)[0];
             mu.Dispose();
             sigma.Dispose();
-
             Cv2.CvtColor(testMat, testMat, ColorConversion.BgraToGray, 0);
             testMat = testMat.GaussianBlur(new OpenCvSharp.CPlusPlus.Size(1, 1), 5, 5, BorderType.Default);
             testMat = testMat.Canny(0.5 * mean, 1.2 * mean, 3, true);
@@ -168,11 +247,23 @@ namespace Microsoft.Samples.Kinect.ColorBasics
             testMat.Dispose();
         }
 
-        public static BitmapSource ConvertBitmap(Bitmap source)
+        private void calculateFps()
         {
-            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(source.GetHbitmap(), IntPtr.Zero,
-                          Int32Rect.Empty,
-                          BitmapSizeOptions.FromEmptyOptions());
+            ticksNow = DateTime.Now.Ticks;
+            fps = ((float)TICKS_PER_SECOND) / (DateTime.Now.Ticks - prevTick); // 1 / ((ticksNow - prevTick) / TICKS_PER_SECOND);
+            //Console.Out.WriteLine("fps: " + fps);
+            Console.Out.WriteLine("fps: " + (int)(fps + 0.5));
+            prevTick = ticksNow;
+
+            //calc mean
+            sumFps += fps;
+            counter++;
+            this.FpsText = "FPS = " + fps.ToString();
+        }
+
+        private BitmapSource ConvertBitmap(Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(source.GetHbitmap(), IntPtr.Zero,Int32Rect.Empty,BitmapSizeOptions.FromEmptyOptions());
         }
 
         /// <summary>
@@ -183,10 +274,7 @@ namespace Microsoft.Samples.Kinect.ColorBasics
         private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
         {
             // on failure, set the status text
-            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
-                                                            : Properties.Resources.SensorNotAvailableStatusText;
-
-
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText : Properties.Resources.SensorNotAvailableStatusText;
         }
 
         /// <summary>
@@ -202,6 +290,14 @@ namespace Microsoft.Samples.Kinect.ColorBasics
             get
             {
                 return this.colorBitmap;
+            }
+        }
+
+        public ImageSource ImageSource2
+        {
+            get
+            {
+                return this.depthBitmap;
             }
         }
 
@@ -230,17 +326,41 @@ namespace Microsoft.Samples.Kinect.ColorBasics
         }
 
         /// <summary>
+        /// Gets or sets the current status text to display
+        /// </summary>
+        public string FpsText
+        {
+            get
+            {
+                return this.fpsText;
+            }
+
+            set
+            {
+                if (this.fpsText != value)
+                {
+                    this.fpsText = value;
+                    // notify any bound elements that the text has changed
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("FpsText"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
         /// Execute shutdown tasks
         /// </summary>
         /// <param name="sender">object sending the event</param>
         /// <param name="e">event arguments</param>
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            if (this.colorFrameReader != null)
+            if (this.multiSourceFrameReader != null)
             {
                 // ColorFrameReder is IDisposable
-                this.colorFrameReader.Dispose();
-                this.colorFrameReader = null;
+                this.multiSourceFrameReader.Dispose();
+                this.multiSourceFrameReader = null;
             }
 
             if (this.kinectSensor != null)
